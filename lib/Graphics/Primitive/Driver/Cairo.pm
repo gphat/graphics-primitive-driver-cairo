1e2d34aaa8c2fc950d4d2e35dfb5827767d47638
Cory G Watson <gphat@onemogin.com>
2008-10-01 21:57:16 +0000

    move tests

--- lib/Graphics/Primitive/Driver/Cairo.pm
+++ lib/Graphics/Primitive/Driver/Cairo.pm
@@ -13,7 +13,7 @@ use Math::Trig ':pi';
 with 'Graphics::Primitive::Driver';
 
 our $AUTHORITY = 'cpan:GPHAT';
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 
 enum 'Graphics::Primitive::Driver::Cairo::AntialiasModes' => (
     qw(default none gray subpixel)
